8dce69b6ce3f37591214179b73a5e53ae97c2aff
Nik68rus <44981471+Nik68rus@users.noreply.github.com>
2019-02-20 15:46:33 +0300

    Подогнал слайдер к макету по пиксель-перфект

--- js/script.js
+++ js/script.js
@@ -2,8 +2,21 @@ var slides = document.querySelectorAll('.promo-slider-list .promo-slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,2000);
 
+/*
+var servSlides = document.querySelectorAll('.service-slider-list .service-slide');
+var currentServSlide = 0;
+var servSlideInterval = setInterval(nextServSlide,2000);
+*/
+
 function nextSlide() {
   slides[currentSlide].className = 'promo-slide';
   currentSlide = (currentSlide+1)%slides.length;
   slides[currentSlide].className = 'promo-slide showing';
 }
+/*
+function nextServSlide() {
+  servSlides[currentServSlide].className = 'service-slide';
+  currentServSlide = (currentServSlide+1)%servSlides.length;
+  servSlides[currentServSlide].className = 'service-slide showing';
+}
+*/
